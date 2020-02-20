9aa130bd02a9a82afada4ecc3309ad0ff8d30b11
Lenz <lenz.kleiser@bluewin.ch>
2020-02-20 22:47:18 +0100

    Updates

---
+++ LWRPTest/Assets/Assets/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private GameObject player;
+
+	private Vector3 offset;
+
+	public float smoothSpeed = 5f;
+	public Vector2 boundaries;
+
+	// Use this for initialization
+	void Start () {
+
+		//Player GameObject.
+		player = GameObject.Find ("Player");
+
+		//Offset of camera relative to player.
+		offset = transform.position - player.transform.position;
+	}
+
+	// Fixed Update is called every couple of frames. Used if an update every frame not needed.
+	void FixedUpdate () {
+
+		//If the player is inside the boundaries of the level.
+        if (transform.position.x > boundaries.x && transform.position.x < boundaries.y) {
+
+			//Set desiredPosition to the position where camera should be.
+            Vector3 desiredPosition = new Vector3(player.transform.position.x, 0, 0) + offset;
+
+			//Set smoothedPosition to a vector in between the current and the desired position.
+			Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+			//Set the camera position to the smoothed position.
+			transform.position = smoothedPosition;
+
+		}
+	}
+}
---
+++ LWRPTest/Assets/Assets/Scripts/WaterScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour
+{
+[Header("References")]
+public Material materialWater;
+public Material materialWater_Foam;
+
+[Header("Properties")]
+public float speedMin;
+public float speedMax;
+
+[Header("Variables")]
+public float currentSpeed;
+public float desiredSpeed;
+
+    void Start()
+    {
+        currentSpeed = speedMin;
+        materialWater.SetVector("FlowSpeed", new Vector4 (0, currentSpeed, 0, 0));
+        materialWater_Foam.SetVector("FlowSpeed", new Vector4 (0, currentSpeed, 0, 0));
+
+        GameController.OnDamChange += OnDamChange;
+    }
+     
+    void OnDamChange() 
+    {
+        desiredSpeed = Mathf.Lerp(speedMin, speedMax, GameController.instance.holeBlend);
+        StartCoroutine(FadeToSpeed(desiredSpeed, materialWater));
+        StartCoroutine(FadeToSpeed(desiredSpeed, materialWater_Foam));
+    }
+
+    IEnumerator FadeToSpeed(float endSpeed, Material material) 
+    {
+        float t = 0;
+        float startSpeed = currentSpeed;
+
+        while (t <= 2) 
+        {
+            t += Time.deltaTime;
+
+            currentSpeed = Mathf.Lerp(startSpeed, endSpeed, t/2);
+            
+            material.SetVector("FlowSpeed", new Vector4 (0, currentSpeed, 0, 0));
+            yield return null;
+        }
+
+        currentSpeed = endSpeed;
+        material.SetVector("FlowSpeed", new Vector4 (0, currentSpeed, 0, 0));
+    }
+}
